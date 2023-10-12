bc96c836d8c82627a3eae2bf78f19d7d57245832
Valer1yaaa <valeria.03gar@mail.ru>
2023-10-13 02:35:49 +0300

    add project

---
+++ Knapsack_problem/Knapsack_problem/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static int Knapsack(int[] weight, int[] value, int capacity)
+    {
+        int n = weight.Length;
+        int[,] dp = new int[n + 1, capacity + 1];
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= capacity; j++)
+            {
+                if (weight[i - 1] <= j)
+                {
+                    dp[i, j] = Math.Max(value[i - 1] + dp[i - 1, j - weight[i - 1]], dp[i - 1, j]);
+                }
+                else
+                {
+                    dp[i, j] = dp[i - 1, j];
+                }
+            }
+        }
+        return dp[n, capacity];
+    }
+    static void Main()
+    {
+        Console.WriteLine("Enter the weights of the items: ");
+        int[] weight = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+        Console.WriteLine("Enter the values of the items: ");
+        int[] value = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+        Console.WriteLine("Enter the capacity of the knapsack: ");
+        int capacity = int.Parse(Console.ReadLine());
+
+        int max_value = Knapsack(weight, value, capacity);
+        Console.WriteLine("Maximum value of items in a Knapsack: " + max_value);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Knapsack_problem
+# Knapsack problem
+The knapsack problem is to find the most valuable combination of items to fit into a knapsack, considering their weights and values. The goal is to maximize the total value while not exceeding the knapsack's weight capacity.
+
+The problem was solved within the framework of the curriculum.
